ea9f405e4df5a50583f44114c4491f554c8a50ad
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-17 14:35:15 +1000

    changed file

--- src/Ex001/Lab001.java
+++ src/Ex001/Lab001.java
@@ -3,6 +3,7 @@ package Ex001;
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("My age is 95");
+        System.out.println("My age is now 96");
     }
 
 }
