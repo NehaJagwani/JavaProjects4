a88328ec3e6da43625171daaa18e425c85da6048
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-17 14:40:13 +1000

    add

--- src/Ex001/Lab002.java
+++ src/Ex001/Lab002.java
@@ -3,5 +3,7 @@ package Ex001;
 public class Lab002 {
     public static void main(String[] args) {
         System.out.println("added a new file");
+        int x=65;
+        System.out.println(x);
     }
 }
